7973b5b884f34e42144d6413c4ca0c317f82d630
Max S <>
2025-12-17 12:41:50 +0100

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
+from setuptools.command.install import install
+from subprocess import check_call
+
+class PreInstallCommand(install):
+    """Pre-installation for installation mode."""
+    def run(self):
+        check_call("apt-get install python3-requests".split())
+        install.run(self)
 
 setup(name='cbpi4-brewfather',
-      version='0.0.4',
-      description='CraftBeerPi Plugin',
-      author='',
+      version='0.1.0',
+      description='CraftBeerPi Plugin for Brewfather integration',
+      author='Max Sidenstjärna',
       author_email='',
-      url='',
+      url='https://github.com/eco37/cbpi4-brewfather',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
